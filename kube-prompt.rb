90ecf57037b44569b92f70aa045e837d2f8231d9
Masashi SHIBATA <c-bata@users.noreply.github.com>
2017-10-16 11:40:52 +0900

    Update kube-prompt.rb

--- kube-prompt.rb
+++ kube-prompt.rb
@@ -1,9 +1,9 @@
 class KubePrompt < Formula
   desc "An interactive kubernetes client featuring auto-complete using go-prompt."
   homepage "https://github.com/c-bata/kube-prompt"
-  url "https://github.com/c-bata/kube-prompt/releases/download/v1.0.2/kube-prompt_v1.0.2_darwin_amd64.zip"
-  sha256 "e69eef852624a1d8b6f017b537c0c916687c9fa9bef55d614800af70f95da2ba"
-  version "1.0.2"
+  url "https://github.com/c-bata/kube-prompt/releases/download/v1.0.3/kube-prompt_v1.0.3_darwin_amd64.zip"
+  sha256 "03d28bf1a97fc6aa9fc76b5ba6dd831936c4efd69d9864e92b03ae1c100c4219"
+  version "1.0.3"
 
   def install
     bin.install "kube-prompt"
