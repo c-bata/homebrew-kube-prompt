3778aa9b9f050aec7c829fa1008a495d4ea6df5c
c-bata <contact@c-bata.link>
2019-08-05 21:35:12 +0900

    Fix hash of v1.0.7

--- kube-prompt.rb
+++ kube-prompt.rb
@@ -2,7 +2,7 @@ class KubePrompt < Formula
   desc "An interactive kubernetes client featuring auto-complete using go-prompt."
   homepage "https://github.com/c-bata/kube-prompt"
   url "https://github.com/c-bata/kube-prompt/releases/download/v1.0.7/kube-prompt_v1.0.7_darwin_amd64.zip"
-  sha256 "bc8e4c62c4ed598757a7929f2eefdd69f0128fb4bcc04751a3466c1978ddc268"
+  sha256 "66a8d9ac929d879c6b59620e6f042831408d886ff5b6f0c5af6dd85fb847da41"
   version "1.0.7"
 
   def install
