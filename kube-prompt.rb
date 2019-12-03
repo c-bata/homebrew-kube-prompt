6ad20d293cc97e5a3db11c046a0803ead003943f
c-bata <contact@c-bata.link>
2019-12-03 15:29:42 +0900

    Release v1.0.10

--- kube-prompt.rb
+++ kube-prompt.rb
@@ -1,9 +1,9 @@
 class KubePrompt < Formula
   desc "An interactive kubernetes client featuring auto-complete using go-prompt."
   homepage "https://github.com/c-bata/kube-prompt"
-  url "https://github.com/c-bata/kube-prompt/releases/download/v1.0.9/kube-prompt_v1.0.9_darwin_amd64.zip"
-  sha256 "9cc0bb386e747bbc1379b8be7e599b890c5616e2cc523e1130a9ba8c708f3b47"
-  version "1.0.9"
+  url "https://github.com/c-bata/kube-prompt/releases/download/v1.0.10/kube-prompt_v1.0.10_darwin_amd64.zip"
+  sha256 "6c4e56a39981fa58b69aeb1a4f5ca24b3b0c03d059ac06e89d4b4bdd31d6bfdc"
+  version "1.0.10"
 
   def install
     bin.install "kube-prompt"
